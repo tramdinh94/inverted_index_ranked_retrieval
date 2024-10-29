d02bb94f1db739113ce2b3d8b74e4fb786576c8a
tramdinh94 <dth.tram94@gmail.com>
2024-10-29 16:43:44 +1100

    indexer draft 2

--- index.py
+++ index.py
@@ -7,66 +7,104 @@ Created on Mon Oct 28 16:07:54 2024
 
 import sys
 import os 
+from collections import namedtuple
+from nltk import word_tokenize
+from nltk import TreebankWordTokenizer
+from nltk import PunktWordTokenizer
+from nktk import WordPunctTokenizer
+from nltk import RegexpTokenizer
 
 
-class doc_pos_index:
-    def __init__(self, docID):
+
+#creating a named tuple with 2 element
+#pos - position of term in document
+#line - the line corresponding to the position
+Pos_Line = namedtuple('Pos_Line', ['pos', 'line'])
+
+
+
+#creating a new dictionary subclass 
+#doc ID: extra attribute
+#add_pos_line: extra method to add a new pos_line to term
+class doc_term_index(dict):
+    def __init__(self, docID, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.docID = docID
-        self.pos_indexes = {}
-    
+        
     #adding positional index list of a term    
-    def add(self, term, pos):
-        if term in self.pos_indexes:
-            self.pos_indexes[term].append(pos)
+    def add_pos_line(self, term, pos, line):
+        pos_line = Pos_Line(pos, line)                
+        if term in self:
+            self[term].append(pos_line)
         else:
-            self.pos_indexes[term] = [pos]
+            self[term] = [pos_line]
     
-    #accessing a term's positional list
-    def term_pos(self, term):
-        return self.pos_indexes[term]
-            
-            
 
-class inverted_index:
-    def __init__(self):
-        self.data = {}     #should be a dictionary of list of dictionary
-    
-    #adding positional data from a doc_pos_index object
-    def add_doc(self, doc):    
-        for term in doc.pos_indexes:
-            if term in self.data: 
-                #find the right position for the doc based on docID
-                #and insert it
-                pass
+            
+            
+#creating a subclass of dict for inverted index
+class inverted_index(dict): 
+       
+    #adding positional data from a doc_term_index object
+    def add_doc(self, doc):
+        #for debug - remove when codes run properly
+        assert isinstance(doc, doc_term_index)
+        for term in doc:
+            if term in self: 
+                self[term][doc.ID] = doc[term]
             else:
-                self.data[term] = [{doc.docID: doc.term_pos(term)}]
-    def print_to_json(self):
+                self[term] = {doc.docID : doc[term]}
+    
+    #print the inverted index to files in given directory
+    def print_to_json(self, index_dir):
         pass
                 
                     
 
 
-def process():
-    #take an unprocessed doc document
-    #return doc_pos_index object
+def custom_tokenise(s):
+    #take an unprocessed string
+    #output list of token
     pass
     
 
 
-doc_dir = sys.argv[1]
-index_dir = sys.argv[2]
+def extract_data(file_name, file_path):
+    doc = doc_term_index(docID = int(file_name))
+    iline = 0
+    itk = 0
+    with open(file_path, 'r') as f:
+        for line in f:
+            iline += 1
+            tokens = custom_tokenise(line)
+            for term in tokens:
+                itk += 1
+                doc.add_pos_line(term, pos = itk, line = iline)
+    return doc
+
+
+
+#doc_dir = sys.argv[1]
+#index_dir = sys.argv[2]
 my_inverted_index = inverted_index()
 
 
+doc_dir = 'data'
+index_dir = 'my_index'
 
-
+#build inverted index
 Files = os.listdir(doc_dir)
-for file in Files: 
-    file_path = os.path.join(doc_dir, file)
-    with open(file_path, 'r') as f:
-        doc_text = f.read()
-        doc = process(doc_text)
-        my_inverted_index.add_doc(doc)
+for file_name in Files: 
+    file_path = os.path.join(doc_dir, file_name)
+    #creating a doc_term_index object
+    doc = extract_data(file_name, file_path)
+    my_inverted_index.add_doc(doc)
+        
+
+#print inverted index
+my_inverted_index.print_to_json(index_dir)        
+
+
 
     
 
